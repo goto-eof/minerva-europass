0d5a71a3a107fb33b3cacd74a4df3fa78930c6a0
Andrei Dodu <andrei.dodu@hotmail.it>
2024-02-29 20:55:16 +0100

    refactor

--- src/main/java/com/andreidodu/minervaeuropass/service/TemplateStrategy.java
+++ src/main/java/com/andreidodu/minervaeuropass/service/TemplateStrategy.java
@@ -2,9 +2,8 @@ package com.andreidodu.minervaeuropass.service;
 
 import com.andreidodu.minervaeuropass.dto.resume.ResumeDTO;
 
-import java.util.Map;
-
 public interface TemplateStrategy {
+
     boolean accept(String templateName);
 
     byte[] execute(ResumeDTO resumeDTO);
